8704b853b6a8d8f381929bb34a42ef33cdaedff6
juyalpriyank <juyalpriyank@gmail.com>
2019-02-28 18:36:41 +0530

    leap commit from office

--- leap/leap.py
+++ leap/leap.py
@@ -1,2 +1,14 @@
 def is_leap_year(year):
-    pass
+    leap_year_bool = False
+    if not year%4:
+#        leap_year_bool = True
+        if not year%100:
+            if not year%400:
+                leap_year_bool = True
+        elif not year%400:
+            leap_year_bool = True
+#        else:
+#            leap_year_bool = False
+    return leap_year_bool
+
+
